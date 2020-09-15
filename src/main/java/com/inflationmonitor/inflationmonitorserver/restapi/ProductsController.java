2ecf12ef7aeec1da0bf72e4d3be41e52d4fb17ee
BartekG <golabkowy@gmail.com>
2020-09-16 01:23:59 +0200

    Example RESTpoints which reads OAuth2 authorization parameters (token + user details)

--- src/main/java/com/inflationmonitor/inflationmonitorserver/restapi/ProductsController.java
+++ src/main/java/com/inflationmonitor/inflationmonitorserver/restapi/ProductsController.java
@@ -1,5 +1,11 @@
 package com.inflationmonitor.inflationmonitorserver.restapi;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -7,6 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductsController {
+
+    @Autowired
+    private OAuth2AuthorizedClientService oAuth2AuthorizedClientService;
 
     @GetMapping(value = {"/test-not-restricted", "/test-not-restricted/{id}"})
     private String testNotRestrictedAccess(@PathVariable(name = "id", required = false) String id,
@@ -18,5 +27,15 @@ public class ProductsController {
     @GetMapping(value = {"/test-restricted"})
     private String testRestrictedAccess() {
         return "Response for not restricted access GET Request endpoint";
+    }
+
+    @GetMapping("/test-oAuth2-token")
+    public void testOAuth2Credentials(OAuth2AuthenticationToken token) {
+        OAuth2AuthorizedClient client = oAuth2AuthorizedClientService.loadAuthorizedClient(token.getAuthorizedClientRegistrationId(), token.getName());
+    }
+
+    @GetMapping("/test-principal")
+    public String testPrincipal(@AuthenticationPrincipal OAuth2User user) {
+        return user.getAttributes().toString();
     }
 }
